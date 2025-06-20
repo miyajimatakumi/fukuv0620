83bdc5393b21712242c5bf4f952d122a93889512
miyajimatakumi <m.miyajima.takumi@gmail.com>
2025-06-20 13:54:23 +0900

    v1

--- fukuv0620/Form1.cs
+++ fukuv0620/Form1.cs
@@ -2,9 +2,40 @@ namespace fukuv0620
 {
     public partial class Form1 : Form
     {
+        static Random random = new Random();
+        int answer = random.Next(51);
+        int count = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count++;
+            MessageBox.Show($"{count}‰ñ–Ú‚Ì’§í");
+            int yosou;
+            yosou = int.Parse(textBox1.Text);
+            if (yosou == answer)
+            {
+                MessageBox.Show("Bingo!!");
+                count = 0;
+                answer = random.Next(51);
+
+            }
+            else if (yosou > answer)
+            {
+                MessageBox.Show("ookikatta");
+            }
+            else
+            {
+                MessageBox.Show("chiisakatta");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
